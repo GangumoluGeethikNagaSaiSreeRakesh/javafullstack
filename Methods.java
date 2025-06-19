a8240febe5a6d000f40628842b979cd11e22df57
GangumoluGeethikNagaSaiSreeRakesh <gnssrakesh@gmail.com>
2025-06-19 12:32:32 +0530

    Create Methods.java

---
+++ Methods.java
@@ -0,0 +1,62 @@
+//without parameters without return value
+
+/*class Methods{
+public void Add()
+{
+System.out.println(10+20);
+}
+static public void main(String[] add)
+{
+Methods m=new Methods();
+m.Add();
+}
+}*/
+
+
+//without parameters with return value
+
+/*class Methods{
+public int Sub()
+{
+return(12-10);
+}
+static public void main(String[] sub)
+{
+Methods m=new Methods();
+int b=m.Sub();
+System.out.println(b);
+}
+}*/
+
+
+
+//with parameters without return value
+
+/*class Methods
+{
+public void Mul(int a,int b)
+{
+System.out.println(a*b);
+}
+public static void main(String[] mul)
+{
+Methods m=new Methods();
+m.Mul(3,3);
+}
+}*/
+
+
+//with parameters with return value
+
+/*class Methods{
+public int Div(int a,int b)
+{
+return(a/b);
+}
+static public void main(String[] div)
+{
+Methods m=new Methods();
+int b=m.Div(14,2);
+System.out.println(b);
+}
+}*/
