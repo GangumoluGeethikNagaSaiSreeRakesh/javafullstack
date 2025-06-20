681964b21ed7409f5b45b5234f3433046280432a
GangumoluGeethikNagaSaiSreeRakesh <gnssrakesh@gmail.com>
2025-06-20 10:48:11 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,153 @@
+//Arithmetic Operators
+/*import java.util.Scanner;
+class operator
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter first number:");
+int a=s.nextInt();
+System.out.print("Enter second number:");
+int b=s.nextInt();
+System.out.println("Add:"+(a+b));
+System.out.println("Subtract:"+(a-b));
+System.out.println("Multiply:"+(a*b));
+if(b!=0)
+{
+System.out.println("Divide:"+(a/b));
+}
+else
+{
+System.out.println("Divide:Cannot divide by zero");
+}
+}
+}*/
+
+
+
+
+// Increment and Decrement Operators
+/*import java.util.Scanner;
+class operator
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter a number:");
+int a=s.nextInt();
+System.out.println("Original:"+a);
+a++;
+System.out.println("After Increment:"+a);
+a--;
+System.out.println("After Decrement:"+a);
+}
+}*/
+
+
+//Equal and Not Equal Operators
+/*import java.util.Scanner;
+class operator
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter first number:");
+int a=s.nextInt();
+System.out.print("Enter second number:");
+int b=s.nextInt();
+System.out.println("a==b:"+(a==b));
+System.out.println("a!=b:"+(a!=b));
+}
+}*/
+
+
+// Check If Two Numbers Are Equal
+/*import java.util.Scanner;
+class operator
+{
+public static void main(String[]args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter first number:");
+int a=s.nextInt();
+System.out.print("Enter second number:");
+int b=s.nextInt();
+if(a==b)
+{
+System.out.println("Numbers are equal");
+}
+else
+{
+System.out.println("Numbers are not equal");
+}
+}
+}*/
+
+
+//Logical AND,OR,NOT Operators
+/*import java.util.Scanner;
+class operator
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter true/false for a:");
+boolean a=s.nextBoolean();
+System.out.print("Enter true/false for b:");
+boolean b=s.nextBoolean();
+System.out.println("a AND b:"+(a&&b));
+System.out.println("a OR b:"+(a||b));
+System.out.println("NOT a:"+(!a));
+}
+*/
+
+
+//Relational Operators
+/*import java.util.Scanner;
+class operator
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter first number:");
+int x=s.nextInt();
+System.out.print("Enter second number:");
+int y=s.nextInt();
+System.out.println("x < y:"+(x<y));
+System.out.println("x <= y:"+(x<=y));
+System.out.println("x > y:"+(x>y));
+System.out.println("x >= y:"+(x>=y));
+}
+}*/
+
+
+// Print Smaller and Larger Number 
+/*import java.util.Scanner;
+class operator
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.print("Enter first number:");
+int a=s.nextInt();
+System.out.print("Enter second number:");
+int b=s.nextInt();
+if(a>b)
+{
+System.out.println("Larger number:"+a);
+System.out.println("Smaller number:"+b);
+}
+else if(b>a)
+{
+System.out.println("Larger number:"+b);
+System.out.println("Smaller number:"+a);
+}
+else
+{
+System.out.println("Both numbers are equal.");
+}
+}
+}*/
+
+
+
